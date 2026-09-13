db3904a7ff7891db179ead614fd3d375b4c344f4
Winston Liang <cywinston.liang@mail.utoronto.ca>
2026-09-13 18:08:54 -0400

    Add: count-connected-components - submission-6

---
+++ Data Structures & Algorithms/count-connected-components/submission-6.py
@@ -0,0 +1,28 @@
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        # Similar to no. islands
+        # 1. Setup
+        neighbours = defaultdict(list)
+        visited = set()
+        for u,v in edges: # note isolated nodes are not in neighbours, so don't use this to loop through all
+            neighbours[u].append(v)
+            neighbours[v].append(u)
+        components = 0
+
+        def bfs(node):
+            q = deque([node])
+            visited.add(node)
+            while q:
+                node = q.popleft()
+                for nei in neighbours[node]:
+                    if nei not in visited:
+                        visited.add(nei)
+                        q.append(nei)
+        
+        for node in range(n): # NOT NEIGHBOURS since isolated nodes not in it
+            if node not in visited:
+                bfs(node)
+                components += 1
+        return components
+            
+
