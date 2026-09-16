6cff1575a50d4cc3f529044c2e889d3659a716fe
Winston Liang <cywinston.liang@mail.utoronto.ca>
2026-09-16 14:23:47 -0400

    Add: find-median-in-a-data-stream - submission-4

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-4.py
@@ -0,0 +1,39 @@
+class MedianFinder:
+    # Idea is: 
+    # Make two heaps.
+    # First heap stores the smaller part with MaxHeap
+    # Second heap stores the bigger part with minHeap
+    # e.g. [1] then [1]
+    # [1,2] then [1] [2]
+    # [1,2,3] then [1,2] [3]
+    # [1,2,3,4] then [1,2] [3,4]
+    # [1,2,3,4,5] then [1,2,3] [4,5]
+    def __init__(self):
+        self.small = []
+        self.large = []
+        
+
+    def addNum(self, num: int) -> None:
+        if self.large and num >= self.large[0]: #if there is large, and num >= smallest in large
+            heapq.heappush(self.large, num)
+        else:
+            heapq.heappush(self.small, -num) # we push if there is no large
+        # Rebalance: if one heap becomes larger than other by more than 1, move top to other heap
+        if len(self.large) - len(self.small) > 1:
+            top = heapq.heappop(self.large)
+            heapq.heappush(self.small, -top)
+        elif len(self.small) - len(self.large) > 1:
+            top = -heapq.heappop(self.small)
+            heapq.heappush(self.large, top)
+        
+    def findMedian(self) -> float:
+        if len(self.large) > len(self.small):
+            median = self.large[0]
+        elif len(self.large) < len(self.small):
+            median = -self.small[0]
+        else:
+            median = (self.large[0] - self.small[0]) / 2.0
+        return median
+        
+        
+        
